ab35192d98199e113bb6fc19d9cb50d9a52739d9
Ringaile Valiaugaite <ringaile@gmail.com>
2021-07-19 08:45:20 +0200

    implemented voter and proposal functionality

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -6,21 +6,59 @@ import "hardhat/console.sol";
 
 contract Voting {
 
+  struct Voter {
+    bool allowedToVote;
+    bool voted;
+    uint votedProposal;
+  }
+
+  struct Proposal {
+    string title;
+    uint votes;
+  }
+
   address public owner;
   
-  mapping(address => uint) votes;
+  mapping(address => Voter) voters;
+  mapping (uint => Proposal) proposals;
+  uint noOfProposals = 0;
 
   constructor() {
     console.log("Deploying a Voting contract");
     owner = msg.sender;
   }
 
-  function getVote(address _voter) public view returns (uint) {
-    return votes[_voter];
+  function addAProposal(string memory _title) public {
+    Proposal memory proposal = Proposal(_title, 0);
+    noOfProposals++;
+    proposals[noOfProposals] = proposal;
   }
 
-  function setVote(uint _vote) public {
-    console.log("Setting a vote '%s' from '%s'", _vote, msg.sender);
-    votes[msg.sender] = _vote;
+  function allowToVote(address _voter) public view {
+    require(msg.sender == owner, "Only the contract owner can allow to vote.");
+    voters[_voter].allowedToVote == true;
+  }
+
+  function vote(uint _vote) public {
+    require(voters[msg.sender].allowedToVote == true, "You are not allowed to vote.");
+    require(voters[msg.sender].voted == false, "You already voted.");
+    voters[msg.sender].votedProposal = _vote;
+    proposals[_vote].votes++;
+  }
+
+  function getWinningProposal() public view returns (uint, string memory){
+    uint maxNoOfVotes = 0;
+    string memory title = "";
+    for (uint i=1; i<=noOfProposals; i++){
+      if (proposals[i].votes > maxNoOfVotes) {
+        maxNoOfVotes = proposals[i].votes;
+        title = proposals[i].title;
+      }
+    }
+    return (maxNoOfVotes, title);
+  }
+
+  function getVote(address _voter) public view returns (uint) {
+    return voters[_voter].votedProposal;
   }
 }
